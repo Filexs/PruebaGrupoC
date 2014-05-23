e6a1dd35f584bb11248fb48558100c86db7343f3
Ryur <ryur_068@hotmail.com>
2014-05-23 16:29:44 +0200

    Rama Álvaro
    Basicamente al darle al boton con este nombre tan seductor salen la
    tabla Empleados

--- ProyectoGrupo/ProyectoGrupo/Alvaro.cs
+++ ProyectoGrupo/ProyectoGrupo/Alvaro.cs
@@ -16,5 +16,20 @@ namespace ProyectoGrupo
         {
             InitializeComponent();
         }
+
+        private void empleadosBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.empleadosBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.northwind_2007DataSet);
+
+        }
+
+        private void Alvaro_Load(object sender, EventArgs e)
+        {
+            // TODO: esta línea de código carga datos en la tabla 'northwind_2007DataSet.Empleados' Puede moverla o quitarla según sea necesario.
+            this.empleadosTableAdapter.Fill(this.northwind_2007DataSet.Empleados);
+
+        }
     }
 }
